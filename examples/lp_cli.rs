5d7950ce149fcb7ecdd437601490ca7591431ec3
heabeounMKTO <heabeoun69zin@gmail.com>
2025-02-08 21:20:58 +0700

    add lang in example

--- examples/lp_cli.rs
+++ examples/lp_cli.rs
@@ -4,12 +4,20 @@ use clap::Parser;
 #[derive(Parser)]
 struct CliArgs {
     #[arg(long)]
-    input_text: String
+    input_text: String,
+    
+    #[arg(long)]
+    lang: Option<String>
 }
 
 fn main(){
    let args = CliArgs::parse(); 
-   let pz = LazyPhonemizer::init(Some("cmn")).unwrap();
+
+  let _sel_lang = match &args.lang {
+        Some(_lang) => _lang,
+        None => "en"
+    };
+   let pz = LazyPhonemizer::init(Some(_sel_lang)).unwrap();
     
 let phonemized = pz.convert_to_phonemes(&args.input_text, lazy_phonememize::phonememizer::PhonemeOutputType::ASCII).unwrap();
 
