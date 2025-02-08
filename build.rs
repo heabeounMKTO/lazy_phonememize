afb0e7f43ab27cee2e839b2a5361fdb42a539447
heabeounMKTO <heabeoun69zin@gmail.com>
2025-02-08 21:25:57 +0700

    fmt

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 use std::env;
-use std::process::Command;
 use std::path::PathBuf;
+use std::process::Command;
 
 fn main() {
     // Run make first
@@ -9,34 +9,43 @@ fn main() {
         .arg("make")
         .output()
         .expect("Failed to execute command");
-    
+
     if !output.status.success() {
-        eprintln!("Make command failed: {}", String::from_utf8_lossy(&output.stderr));
+        eprintln!(
+            "Make command failed: {}",
+            String::from_utf8_lossy(&output.stderr)
+        );
         std::process::exit(1);
     }
 
     let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    
+
     println!("cargo:rustc-link-search={}", manifest_dir.display());
     let libs_dir = manifest_dir.join("libs");
     println!("cargo:rustc-link-search={}", libs_dir.display());
     println!("cargo:rustc-link-search={}", libs_dir.join("lib").display());
     println!("cargo:rustc-link-search={}", libs_dir.join("bin").display());
-    
+
     // Add PKG_CONFIG_PATH for the custom lib location, THAT WE BUILT FROM SOURCE
     let lib_pkg_config = libs_dir.join("lib").join("pkgconfig");
     if lib_pkg_config.exists() {
-        println!("cargo:rustc-env=PKG_CONFIG_PATH={}", lib_pkg_config.display());
+        println!(
+            "cargo:rustc-env=PKG_CONFIG_PATH={}",
+            lib_pkg_config.display()
+        );
     }
     println!("cargo:rustc-link-lib=static=phonememize");
     println!("cargo:rustc-link-lib=espeak-ng");
-    
+
     // Tell cargo to look for the header files in the custom include directory
     println!("cargo:include={}", libs_dir.join("include").display());
-    
+
     // Tell cargo to rerun if any of these changes
     println!("cargo:rerun-if-changed=Makefile");
     println!("cargo:rerun-if-changed=libs/lib/libphonememize.a");
     println!("cargo:rerun-if-changed=libs/lib/libespeak-ng.a");
-    println!("cargo:rustc-env=LD_LIBRARY_PATH={}", libs_dir.join("lib").display());
+    println!(
+        "cargo:rustc-env=LD_LIBRARY_PATH={}",
+        libs_dir.join("lib").display()
+    );
 }
--- src/phonememizer.rs
+++ src/phonememizer.rs
@@ -1,90 +1,101 @@
-use std::ffi::{CString, CStr};
-use anyhow::{Result,  anyhow};
+use anyhow::{anyhow, Result};
+use std::ffi::{CStr, CString};
 use std::sync::Once;
-
 
 static INIT_G2P_LANG: Once = Once::new();
 
-#[link(name="phonememize", kind="static")]
+#[link(name = "phonememize", kind = "static")]
 extern "C" {
     fn g2p_initialize(voice: *const libc::c_char) -> libc::c_int;
     fn g2p_terminate();
-    fn text2phoneme(input: *const libc::c_char,
-                    output_buffer: *mut libc::c_char,
-                    buffer_size: libc::c_int,
-                    phoneme_mode: libc::c_int) -> libc::c_int;
+    fn text2phoneme(
+        input: *const libc::c_char,
+        output_buffer: *mut libc::c_char,
+        buffer_size: libc::c_int,
+        phoneme_mode: libc::c_int,
+    ) -> libc::c_int;
 }
 
-/// FROM ESPEAK 
+/// FROM ESPEAK
 ///
-/**  
+/**
 * phoneme_mode
-	    bit 1:   0=eSpeak's ascii phoneme names, 1= International Phonetic Alphabet (as UTF-8 characters).
+        bit 1:   0=eSpeak's ascii phoneme names, 1= International Phonetic Alphabet (as UTF-8 characters).
         bit 7:   use (bits 8-23) as a tie within multi-letter phonemes names
         bits 8-23:  separator character, between phoneme names
 **/
 pub enum PhonemeOutputType {
     ESpeak,
-    ASCII
+    ASCII,
 }
-
 
 pub struct LazyPhonemizer {
     pub lang: String,
-    pub init_state: bool
-} 
+    pub init_state: bool,
+}
 
 impl LazyPhonemizer {
     pub fn init(lang: Option<&str>) -> Result<LazyPhonemizer> {
         let _init_lang = match lang {
             Some(_lang) => _lang,
-            None =>"en"
-       };
+            None => "en",
+        };
 
         let mut init_ok = Ok(());
-        let _lang_ptr = CString::new(_init_lang).map_err(|_| "wtf invalid lang input").unwrap();
-        INIT_G2P_LANG.call_once(|| {
-            unsafe {
-                if  g2p_initialize(_lang_ptr.as_ptr()) < 0 {
-                    init_ok = Err("Failed to init g2p".to_string());
-                }
+        let _lang_ptr = CString::new(_init_lang)
+            .map_err(|_| "wtf invalid lang input")
+            .unwrap();
+        INIT_G2P_LANG.call_once(|| unsafe {
+            if g2p_initialize(_lang_ptr.as_ptr()) < 0 {
+                init_ok = Err("Failed to init g2p".to_string());
             }
         });
         Ok(LazyPhonemizer {
             lang: String::from(_init_lang),
             init_state: match init_ok {
                 Ok(_) => true,
-                _ => false
-            }
+                _ => false,
+            },
         })
     }
 
-    pub fn convert_to_phonemes(&self, text: &str, output_type: PhonemeOutputType) -> Result<String> {
+    pub fn convert_to_phonemes(
+        &self,
+        text: &str,
+        output_type: PhonemeOutputType,
+    ) -> Result<String> {
         let output_type_int: i32 = match output_type {
             PhonemeOutputType::ESpeak => 0,
-            PhonemeOutputType::ASCII => 2
+            PhonemeOutputType::ASCII => 2,
         };
-        let c_text = CString::new(text).map_err(|_| "wtf invalid text input").unwrap();
-        // 
-        /* wtf is a 0u8? 
-         https://stackoverflow.com/questions/53120755/what-do-number-literals-with-a-suffix-like-0u8-mean-in-rust 
+        let c_text = CString::new(text)
+            .map_err(|_| "wtf invalid text input")
+            .unwrap();
+        //
+        /* wtf is a 0u8?
+         https://stackoverflow.com/questions/53120755/what-do-number-literals-with-a-suffix-like-0u8-mean-in-rust
         */
         // println!("[DEBUG] text len {}", text.chars().count());
         let mut buffer = vec![0u8; text.chars().count() * 8]; // big ass buffer for no reason
-        println!("[DEBUG] [LazyPhonemizer] `lazy_p buffer len {:?}", &buffer.len());
+        println!(
+            "[DEBUG] [LazyPhonemizer] `lazy_p buffer len {:?}",
+            &buffer.len()
+        );
         unsafe {
-            let result = text2phoneme(c_text.as_ptr(), 
-                                      buffer.as_mut_ptr() as *mut libc::c_char, 
-                                      buffer.len() as i32, 
-                                      output_type_int);
+            let result = text2phoneme(
+                c_text.as_ptr(),
+                buffer.as_mut_ptr() as *mut libc::c_char,
+                buffer.len() as i32,
+                output_type_int,
+            );
             // println!("[DEBUG] conversion REUSLT {}", result);
             if result < 0 {
-               return Err(anyhow!("conversion failed with error code {}", result));
+                return Err(anyhow!("conversion failed with error code {}", result));
             }
             let result_str = CStr::from_ptr(buffer.as_ptr() as *const libc::c_char);
             Ok(result_str.to_string_lossy().into_owned())
         }
-}
+    }
 }
 impl Drop for LazyPhonemizer {
     fn drop(&mut self) {
@@ -93,6 +104,3 @@ impl Drop for LazyPhonemizer {
         }
     }
 }
-
-
-
